a8986d8cabf9e22cc3877da82205967fb328e7bc
Matt Smith <matt.smith@uniphore.com>
2024-11-19 18:41:00 +0000

    init

---
+++ TripPlanner/TripPlanner.Server/Controllers/TripsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TripPlanner.Server.Data;
+
+namespace TripPlanner.Server.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class TripsController : Controller
+    {
+        private readonly TripPlannerDbContext _context;
+
+        public TripsController(TripPlannerDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTrips()
+        {
+            var trips = await _context.Trips.ToListAsync();
+
+            return Ok(trips);
+        }
+    }
+}
